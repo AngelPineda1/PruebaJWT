ca33a38430405679e5c2324dd9b06f74326f2e08
AngelPineda1 <201G0276@rcarbonifera.tecnm.mx>
2024-05-09 17:34:58 -0600

    Add project files.

---
+++ PruebaJWT/Controllers/LoginController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PruebaJWT.Helpers;
+
+namespace PruebaJWT.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Post(string username, string password)
+        {
+            if (password == "ITESRC")
+            {
+                JwtTokenGenerator tokenGenerator = new JwtTokenGenerator();
+                return Ok(tokenGenerator.GetToken(username));
+            }
+            return Unauthorized();  
+        }
+    }
+}
---
+++ PruebaJWT/Helpers/JwtTokenGenerator.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace PruebaJWT.Helpers
+{
+    public class JwtTokenGenerator
+    {
+        public string GetToken(string nombre)
+        {
+            List<Claim> claims = new List<Claim>();
+            claims.Add(new Claim("Rol", "Admin"));
+            claims.Add(new Claim(ClaimTypes.Name, nombre));
+            claims.Add(new Claim(JwtRegisteredClaimNames.Iss, "Saludos"));
+            claims.Add(new Claim(JwtRegisteredClaimNames.Aud, "prueba"));
+            claims.Add(new Claim(JwtRegisteredClaimNames.Iat, DateTime.Now.ToString()));
+            claims.Add(new Claim(JwtRegisteredClaimNames.Exp, DateTime.Now.AddMinutes(5).ToString()));
+            JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();
+            var token = new SecurityTokenDescriptor()
+            {
+                Subject = new ClaimsIdentity(claims),
+                Issuer = "Saludos",
+                Audience = "prueba",
+                IssuedAt = DateTime.Now,
+                Expires = DateTime.Now.AddMinutes(5),
+                NotBefore = DateTime.Now.AddMinutes(-1),
+                SigningCredentials=new SigningCredentials(new SymmetricSecurityKey(System.Text.Encoding.UTF8
+            .GetBytes("ESTAESMILLAVEDECIFRADODELTOKEN2024")),SecurityAlgorithms.HmacSha256)
+            };
+
+
+            return handler.CreateEncodedJwt(token);
+        }
+    }
+}
